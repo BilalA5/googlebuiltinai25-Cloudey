83b25abbfbddd4d760494558eb2b4f4ad44595e6
bahmed721 <s.bilal.ahmed927@gmail.com>
2025-10-11 00:50:47 -0600

    Added Shader animation and started on Landing Page.

--- landing-page/app/routes/home.tsx
+++ landing-page/app/routes/home.tsx
@@ -1,13 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/landingPage";
+import  LandingPage  from "../welcome/landingPage";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App arash ajdari is a noob 123" },
-    { name: "description", content: "Welcome to React Router arash ajdari is a noob 123s!" },
+    { title: "exTendifAI" },
+    { name: "description", content: "Landing Page for Chrome Extension" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <LandingPage />;
 }
